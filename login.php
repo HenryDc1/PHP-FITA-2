a2531746e3b33942d2c7809388b863d818f5df38
HenryDc1 <henydoudoc@gmail.com>
2023-09-21 20:12:46 +0200

    fita 2

---
+++ login.php
@@ -0,0 +1,23 @@
+<h2>Login</h2>
+
+<form >
+    <input type="text" name="user" placeholder="user">
+    <br>
+    <input type="password" name="pass" placeholder="password">
+    <br>
+    <input type="submit" value="Submit">
+</form>
+<?php
+$usuaris=array(
+    "Henry"=>"123",
+    "Pepe" => "321"
+);
+if (isset($_GET["user"])) {
+    echo "Usuari:".$_GET["user"]."<br>\n";
+    echo "Password:".$_GET["pass"]."<br>\n";
+    # code...
+}
+else {
+    echo "Usuari:".$_GET["user"]."<br>\n";
+}
+echo $usuaris["Henry"];
